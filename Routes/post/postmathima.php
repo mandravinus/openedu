19ba13f276963478ea0707816bc7c1fdb60baec5
Polichronucci <nick@discloud.eu>
2017-07-14 13:55:20 +0300

    Request type must be uppercase

--- Routes/post/postmathima.php
+++ Routes/post/postmathima.php
@@ -114,13 +114,13 @@ $app->post('/mathima', function($request, $response) use ($diy_storage, $diy_res
 					$TITLOS = $dget["email"];
 
 					$data_json =  '{ "title": "'.$TITLOS.'", "status":"pending" }';
-					$exec = 'curl -k --header "Authorization: Basic '.$restapitmp.'" -H "Content-Type: application/json" -X post  '.$restapipoint.' -d '."'".$data_json."'";
+					$exec = 'curl -k --header "Authorization: Basic '.$restapitmp.'" -H "Content-Type: application/json" -X POST  '.$restapipoint.' -d '."'".$data_json."'";
 
 					$exec .= " | jq '.id' 2>&1";
 					exec($exec, $output, $return_var);
 					$fields1['fields']=$fields;
 					$content1 = json_encode($fields1);
-					$exec1 = 'curl -k --header "Authorization: Basic '.$restapitmp.'" -H "Content-Type: application/json" -X post  '.$restapipoint2.'/${'.$output[0].'} -d '."'".$content1."'";
+					$exec1 = 'curl -k --header "Authorization: Basic '.$restapitmp.'" -H "Content-Type: application/json" -X POST  '.$restapipoint2.'/${'.$output[0].'} -d '."'".$content1."'";
 
 					$exec1 .= " 2>&1";
 					exec($exec1, $output1, $return_var1);
